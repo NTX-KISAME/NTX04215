55529fa544c34f9e5059733c78f9b4227e893850
Kisame/NatsuX <93478433+NTX-KISAME@users.noreply.github.com>
2021-11-01 14:47:44 +0800

    Main Files 85% Done

---
+++ src/precache-manifest.fac146d7661893b4ee001336b4381102.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224b50dfffd4d20a970d",
+    "url": "/css/app.c69ab527.css"
+  },
+  {
+    "revision": "65f6b5381a961037bbedde3b15a1ee16",
+    "url": "/img/achivements.65f6b538.jpg"
+  },
+  {
+    "revision": "7ab76662ab676bef9494be7b695a19c8",
+    "url": "/img/profilebg.7ab76662.jpg"
+  },
+  {
+    "revision": "9fdbe69a2e1fedf0e59a2624ff762ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "224b50dfffd4d20a970d",
+    "url": "/js/app.d1cd7196.js"
+  },
+  {
+    "revision": "b173b7d535989dd29343",
+    "url": "/js/chunk-vendors.6a01da97.js"
+  },
+  {
+    "revision": "57658f8bd13d323aec2866f992aed0ee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "479d6817a3db480035f89e21ef1bc30d",
+    "url": "/media/404.479d6817.mp4"
+  },
+  {
+    "revision": "06f3049140e8c0d74a392d922b53d399",
+    "url": "/media/bgmvid.06f30491.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
